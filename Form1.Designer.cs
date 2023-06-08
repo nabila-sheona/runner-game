4fb520325181ca3ac34b2c89c7ac9f8697eb37a1
Nabila Sheona <126275965+sirexcc001@users.noreply.github.com>
2023-06-08 18:48:30 +0600

    runner game

---
+++ Form1.Designer.cs
@@ -0,0 +1,213 @@
+﻿namespace runner_game
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.score = new System.Windows.Forms.Label();
+            this.gametimer = new System.Windows.Forms.Timer(this.components);
+            this.distance = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.avatar = new System.Windows.Forms.PictureBox();
+            this.obstacle1 = new System.Windows.Forms.PictureBox();
+            this.obstacle2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.avatar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.obstacle1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.obstacle2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // score
+            // 
+            this.score.AutoSize = true;
+            this.score.BackColor = System.Drawing.Color.Black;
+            this.score.Font = new System.Drawing.Font("MS Reference Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.score.ForeColor = System.Drawing.Color.White;
+            this.score.Location = new System.Drawing.Point(12, 412);
+            this.score.Name = "score";
+            this.score.Size = new System.Drawing.Size(123, 29);
+            this.score.TabIndex = 5;
+            this.score.Text = "Score: 0";
+            this.score.Click += new System.EventHandler(this.score_Click);
+            // 
+            // gametimer
+            // 
+            this.gametimer.Interval = 20;
+            this.gametimer.Tick += new System.EventHandler(this.timeevent);
+            // 
+            // distance
+            // 
+            this.distance.AutoSize = true;
+            this.distance.BackColor = System.Drawing.Color.Black;
+            this.distance.Font = new System.Drawing.Font("MS Reference Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.distance.ForeColor = System.Drawing.Color.White;
+            this.distance.Location = new System.Drawing.Point(545, 415);
+            this.distance.Name = "distance";
+            this.distance.Size = new System.Drawing.Size(161, 29);
+            this.distance.TabIndex = 6;
+            this.distance.Text = "Distance: 0";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::runner_game.Properties.Resources._360_F_477440329_x4AOe1grWqyOQu335eqsifroDiVefft0;
+            this.pictureBox2.Location = new System.Drawing.Point(12, 254);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 8;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::runner_game.Properties.Resources.istockphoto_1124567572_612x612;
+            this.pictureBox1.Location = new System.Drawing.Point(17, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 84);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 7;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.BackColor = System.Drawing.Color.Black;
+            this.pictureBox4.Location = new System.Drawing.Point(0, 407);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(2000, 50);
+            this.pictureBox4.TabIndex = 3;
+            this.pictureBox4.TabStop = false;
+            // 
+            // avatar
+            // 
+            this.avatar.BackColor = System.Drawing.Color.Transparent;
+            this.avatar.Image = global::runner_game.Properties.Resources.running;
+            this.avatar.Location = new System.Drawing.Point(151, 360);
+            this.avatar.Name = "avatar";
+            this.avatar.Size = new System.Drawing.Size(53, 52);
+            this.avatar.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.avatar.TabIndex = 4;
+            this.avatar.TabStop = false;
+            // 
+            // obstacle1
+            // 
+            this.obstacle1.BackColor = System.Drawing.Color.Transparent;
+            this.obstacle1.Image = global::runner_game.Properties.Resources.obstacle_1;
+            this.obstacle1.Location = new System.Drawing.Point(535, 371);
+            this.obstacle1.Name = "obstacle1";
+            this.obstacle1.Size = new System.Drawing.Size(34, 41);
+            this.obstacle1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.obstacle1.TabIndex = 2;
+            this.obstacle1.TabStop = false;
+            // 
+            // obstacle2
+            // 
+            this.obstacle2.BackColor = System.Drawing.Color.Transparent;
+            this.obstacle2.Image = global::runner_game.Properties.Resources.obstacle_2;
+            this.obstacle2.Location = new System.Drawing.Point(667, 371);
+            this.obstacle2.Name = "obstacle2";
+            this.obstacle2.Size = new System.Drawing.Size(49, 41);
+            this.obstacle2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.obstacle2.TabIndex = 1;
+            this.obstacle2.TabStop = false;
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.Image = global::runner_game.Properties.Resources._360_F_477440329_x4AOe1grWqyOQu335eqsifroDiVefft0;
+            this.pictureBox5.Location = new System.Drawing.Point(667, 291);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox5.TabIndex = 10;
+            this.pictureBox5.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::runner_game.Properties.Resources._360_F_477440329_x4AOe1grWqyOQu335eqsifroDiVefft0;
+            this.pictureBox3.Location = new System.Drawing.Point(455, 46);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 11;
+            this.pictureBox3.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(768, 450);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.score);
+            this.Controls.Add(this.distance);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.avatar);
+            this.Controls.Add(this.obstacle1);
+            this.Controls.Add(this.obstacle2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.keyisdown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.keyisup);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.avatar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.obstacle1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.obstacle2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.PictureBox obstacle2;
+        private System.Windows.Forms.PictureBox obstacle1;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox avatar;
+        private System.Windows.Forms.Label score;
+        public System.Windows.Forms.Timer gametimer;
+        private System.Windows.Forms.Label distance;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.PictureBox pictureBox3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Media;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace runner_game
+{
+    public partial class Form1 : Form
+    {
+        bool jumping = false;
+        int jumpspeed = 12;
+        int force = 15;
+        int sscore = 0;
+        int ddistance = 0;
+        int obstaclespeed = 5;
+        Random random = new Random();
+        int position;
+        bool isgameover = false;
+        public Form1()
+        {
+            InitializeComponent();
+            // Create a SoundPlayer instance
+            SoundPlayer player = new SoundPlayer();
+
+            // Set the path to your background music file
+            player.SoundLocation = "D:\\freshman summer\\runner game\\Resources\\Aurora_-_Runaway_CeeNaija.com_.wav";
+
+            // Play the music
+            player.PlayLooping();
+            gamereset();
+
+        }
+
+        private void timeevent(object sender, EventArgs e)
+        {
+            avatar.Top += jumpspeed;
+            obstacle1.Left -= obstaclespeed;
+            obstacle2.Left -= obstaclespeed;
+            score.Text = "Score: " + sscore;
+
+
+
+
+            if (obstacle1.Left < -100)
+            {
+
+               
+                obstacle1.Left = random.Next(300, 1200);
+                // pipebottom.Top += 30;
+                //pipebottom.Top -= 50;
+                //&& pipespeed>13
+
+                sscore++;
+                score.Text = "Score: " + sscore;
+            }
+
+           
+
+            if (obstacle2.Left < -130)
+            {
+               
+                obstacle2.Left = obstacle1.Left+ random.Next(300, 1200);
+                sscore++;
+                score.Text = "Score: " + sscore;
+
+            }
+
+
+            ddistance++;
+            distance.Text = "Distance: " + ddistance;
+
+            if (jumping==true && force < 0)
+            {
+                jumping = false;
+            }
+
+            if (jumping==true)
+            {
+                jumpspeed = -12;
+                force -= 1;
+            }
+            else
+            {
+                
+                jumpspeed = 12;
+            }
+            
+            if(avatar.Top>229  && jumping == false)
+            {
+                force = 25;
+                avatar.Top = 230;
+                jumpspeed = 0;
+            }
+    
+
+            if (avatar.Bounds.IntersectsWith(obstacle1.Bounds) || avatar.Bounds.IntersectsWith(obstacle2.Bounds))
+            {
+
+                // we stop the timer
+                gametimer.Stop();
+                score.Text = "Score: " + sscore;
+                // change the t rex image to the dead one
+                avatar.Image = Properties.Resources.dead;
+                // show press r to restart on the score text label
+                score.Text += "  Press R to restart";
+                isgameover = true;
+            }
+
+
+            if (ddistance > 1200)
+            {
+                obstaclespeed = 8;
+            }
+            else if (ddistance > 2200)
+            {
+                obstaclespeed=10;
+            }
+            
+
+            
+        }
+
+        private void keyisdown(object sender, KeyEventArgs e)
+        {
+            
+            if (e.KeyCode == Keys.Space && jumping==false)
+            {
+                jumping = true;
+            }
+            
+
+        }
+
+        private void keyisup(object sender, KeyEventArgs e)
+        {
+            
+            if (jumping==true)
+            {
+                jumping = false;
+            }
+
+
+            if (e.KeyCode == Keys.R && isgameover==true)
+            {
+                gamereset();
+            }
+
+
+        }
+        private void gamereset()
+        {
+           
+            force = 15;
+            jumpspeed = 0;
+            jumping = false;
+            sscore = 0;
+            ddistance = 0;
+            obstaclespeed = 5;
+            score.Text = "Score: " + sscore;
+            distance.Text = "Distance: 0";
+            avatar.Image = Properties.Resources.running;
+            isgameover = false;
+            avatar.Top = 230;
+
+           
+                    // generate a random number in the position integer between 600 and 1000
+                    position =random.Next(500,800);
+                    // change the obstacles left position to a random location begining of the game
+                    obstacle1.Left =position;
+            obstacle2.Left = random.Next(600, 1000);               
+            
+            gametimer.Start();
+        }
+
+        private void score_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+}
